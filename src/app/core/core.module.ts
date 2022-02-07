2a421bfd27fa2eaaab096e0cff803b150f7e8683
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2022-02-07 16:11:32 +0100

    7 - transclusion + ng-content

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,11 +4,18 @@ import { UiModule } from '../ui/ui.module';
 import { IconsModule } from '../icons/icons.module';
 import { TemplatesModule } from '../templates/templates.module';
 import { LoginModule } from '../login/login.module';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    NavComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ],
@@ -16,7 +23,10 @@ import { LoginModule } from '../login/login.module';
     UiModule,
     IconsModule,
     TemplatesModule,
-    LoginModule
+    LoginModule,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent
   ] 
 })
 export class CoreModule { }
