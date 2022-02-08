334fcb7caa5b364ad590296c78de1f4eb3e57411
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2022-02-08 14:50:56 +0100

    15 - routes orders client 404 + piège import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'formation-angular-scalian';
   private testPrivate = 'Ceci n\'est pas accessible dans le template'; 
+
+  constructor(private router: Router) {
+    console.log("router : ", router.config);
+  }
 }
