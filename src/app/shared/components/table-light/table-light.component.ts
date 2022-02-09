78a7902e35872d0eb420c2e2754dc62c7a56ea2b
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2022-02-09 16:06:47 +0100

    25 - projection tableau générique

--- src/app/shared/components/table-light/table-light.component.ts
+++ src/app/shared/components/table-light/table-light.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Order } from 'src/app/core/models/order';
 
 @Component({
   selector: 'app-table-light',
@@ -8,25 +7,9 @@ import { Order } from 'src/app/core/models/order';
 })
 export class TableLightComponent implements OnInit {
   @Input() public headers!: string[];
-  @Input() public collection!: Order[];
-
-  constructor() {
-    console.log("---- instanciation",);
-    console.log("----", this.headers);
-    console.log("----", this.collection);
-   }
-
-  ngOnChanges(): void {
-    console.log("---- Change",);
-    console.log("----", this.headers);
-    console.log("----", this.collection);
-  }
+  
+  constructor() {}
 
   ngOnInit(): void {
-    console.log("---- Init",);
-    console.log("----", this.headers);
-    console.log("----", this.collection);
   }
-  
-
 }
