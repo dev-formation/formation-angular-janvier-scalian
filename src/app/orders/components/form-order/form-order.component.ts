6fdf146aabd433e5ef28a86b70d8bf2f78948ec3
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2022-02-10 14:58:05 +0100

    31 - reactiveform + formbuilder+ formcontrol/group

--- src/app/orders/components/form-order/form-order.component.ts
+++ src/app/orders/components/form-order/form-order.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { StateOrder } from 'src/app/core/enums/state-order';
 import { Order } from 'src/app/core/models/order';
 
@@ -10,10 +11,32 @@ import { Order } from 'src/app/core/models/order';
 export class FormOrderComponent implements OnInit {
   public states = StateOrder;
   @Input() init!: Order;
-  
-  constructor() { }
+  public form!:FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    // V1 à la manière de la doc
+    // this.form = new FormGroup({
+    //   tjmHt: new FormControl(this.init.tjmHt),
+    //   type: new FormControl(this.init.typePresta),
+    //   nbJours: new FormControl(this.init.nbJours),
+    //   client: new FormControl(this.init.client),
+    //   tva: new FormControl(this.init.tva),
+    //   state: new FormControl(this.init.state),
+    //   comment: new FormControl(this.init.comment)
+    // });
+
+    // manière concise
+    this.form = this.fb.group({
+      tjmHt: [this.init.tjmHt],
+      type: [this.init.typePresta],
+      nbJours: [this.init.nbJours],
+      client: [this.init.client],
+      tva: [this.init.tva],
+      state: [this.init.state],
+      comment: [this.init.comment]
+    })
   }
 
 }
