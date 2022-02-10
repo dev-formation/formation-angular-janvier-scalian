28c3522e12d0bb601b67235cd26cfcf17a82c873
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2022-02-10 09:53:24 +0100

    26.1 - unsubscribe + pipe async

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 import { Order } from 'src/app/core/models/order';
+import { VersionService } from 'src/app/core/services/version.service';
 import { OrdersService } from '../../services/orders.service';
 
 @Component({
@@ -10,7 +12,9 @@ import { OrdersService } from '../../services/orders.service';
 })
 export class PageListOrdersComponent implements OnInit {
   public titleParent = 'Liste de commandes';
-  public collection!: Order[];
+  // public collection!: Order[];
+  public collection$!: Observable<Order[]>;
+  public numVersion$: BehaviorSubject<number>;
   public headers: string[];
 
   public titleTest = 'Le titre de mon composant';
@@ -18,25 +22,26 @@ export class PageListOrdersComponent implements OnInit {
   public userList!: any;
   public userListHeaders!: string[];
   public demoDate = new Date();
+  
   private count = 0;
+  // private subNumVersion: Subscription;
 
-  constructor(private ordersService: OrdersService, private http: HttpClient) { 
+  constructor(private ordersService: OrdersService, private versionService: VersionService) { 
     this.headers = ["TjmHt", "NbJours", "TVA", "Total HT", "Total TTC", "Type Presta", "Client", "State"];
     
-    this.ordersService.collection$.subscribe({
-        next: (data) => { 
-          console.log('Next : ', data);
-          this.collection = data;
-        },
-        error: (err) => { console.error('Error : ', err)},
-        complete: () => { console.info('Fin de transmission')}
-      })
+    this.collection$ = this.ordersService.collection$;
+
+    // this.ordersService.collection$.subscribe({
+    //     next: (data) => { 
+    //       console.log('Next : ', data);
+    //       this.collection = data;
+    //     },
+    //     error: (err) => { console.error('Error : ', err)},
+    //     complete: () => { console.info('Fin de transmission')}
+    //   })
       
-      //Experiment api public + table component
-      // this.http.get('https://reqres.in/api/users').subscribe((resp: any) => {
-      //   this.userList = resp.data;
-      //   this.userListHeaders = ['id', 'email', 'prenom', 'nom'];
-      // })
+      // this.subNumVersion = this.versionService.numVersion$.subscribe(versionNum => console.warn("**** Num Version"))
+      this.numVersion$ = this.versionService.numVersion$;
   }
 
   public total(val: number, coef: number, tva?: number): number {
@@ -54,5 +59,7 @@ export class PageListOrdersComponent implements OnInit {
   }
 
   ngOnDestroy(): void {
+    console.log('Instance detruite + desinscription');
+    // this.subNumVersion.unsubscribe();
   }
 }
